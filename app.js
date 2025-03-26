0fd34dbd2f32a3f052dba00af5f51415c9aa7f08
nosheenariaz <nosheenariaz123@gmail.com>
2025-03-26 16:22:53 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log('Hello World');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question("What's your name? ", nosheena riaz => {
+  if(!nosheena riaz.trim()) {
+    console.log("You didn't enter a name!");
+  } else {
+    console.log(`Hello, ${nosheena riaz}!`);
+  }
+  readline.close();
+});
